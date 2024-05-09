2f5eb3132bf9ea4a7e02995d2e7762c315770e5c
Maryam Nasir <maryamnasir555@yahoo.com>
2024-05-09 09:39:28 +0500

    Add theme colors in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "hsl(25, 97%, 53%)",
+        white: "hsl(0, 0%, 100%)",
+        lightGrey: "hsl(217, 12%, 63%)",
+        mediumGrey: "hsl(216, 12%, 54%)",
+        darkBlue: "hsl(213, 19%, 18%)",
+        cardBg: "hsl(218, 21%, 15%)",
+        blackishBlue: "hsl(216, 12%, 8%)",
+      },
+    },
   },
   plugins: [],
 };
